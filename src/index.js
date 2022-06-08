a38ecc27f9d4656c6e7c5cff5f6e6ee9d4118a0b
kateryna klimchuk <ketyclima@gmail.com>
2022-06-08 21:28:58 +0300

    searching fn

--- src/index.js
+++ src/index.js
@@ -17,27 +17,38 @@ inputEl.addEventListener('input', onInputType)
 
 function onInputType(event) {
     inputText = inputEl.value;
+    console.log(inputText);
     // const inputText = event.currentTarget.searchQuery.value.trim();
 }
 
 function onFormBtnClick(event) {
     event.preventDefault();
-    fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`).then((response) => {
+    clearGallery();
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${inputText}`).then((response) => {
         return response.json();
     })
         .then(({ results }) => {
-            console.log(results);
-            const resTitle = results.map(({ title, backdrop_path, overview, release_date, genre_ids }) => {
-                return `<div class="gallery-item">
-        <a class="gallery-link" href=""><img class="gallery-img" src=${backdrop_path} alt="${title}"></a>
+console.log(results);
+
+            const film = results.map(({ title, poster_path, overview, release_date, genre_ids }) => {
+            const dateOf = release_date.slice(0, 4);
+            const genreName = [];
+            for (const el of genres) {
+                for (const gen of genre_ids) {
+                    if (el.id === gen) {
+                        genreName.push(el.name)
+                    }
+                }
+            }
+            return `<div class="gallery-item">
+        <a class="gallery-link" href=""><img class="gallery-img" src="https://image.tmdb.org/t/p/w300${poster_path}" alt="${title}" /></a>
         <div class="gallery-info">
             <p class="gallery-name">${title}</p>
-            <p class="gallery-about">${release_date}, ${genre_ids}</p>
+            <p class="gallery-about">${genreName} | ${dateOf}</p>
         </div>
     </div>`}).join('');
 
-            console.log(resTitle);
-            return galleryEl.insertAdjacentHTML('beforeend', resTitle)
+            return galleryEl.insertAdjacentHTML('beforeend', film)
         });
     
 }
@@ -60,9 +71,6 @@ function startPageWithTrendingMovies() {
                     }
                 }
             }
-
-
-
             return `<div class="gallery-item">
         <a class="gallery-link" href=""><img class="gallery-img" src="https://image.tmdb.org/t/p/w300${poster_path}" alt="${title}" /></a>
         <div class="gallery-info">
@@ -75,30 +83,6 @@ function startPageWithTrendingMovies() {
     })
 }
 
-
-// function searchGenresName(...genreId) {
-//     fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=en-US`)
-//         .then((res) => {
-//             return res.json()
-//         })
-//         .then((data) => {
-//             const genreName = data.genres;
-//             genreName.forEach(el => {
-//                 for (let index = 0; index < genreId.length; index++) {
-//                     const element = genreId[index];
-//                     if (el.id === element) {
-//                         // genres.push(el.name);
-//                         // let uniqe = genres.filter((item, i, ar) => ar.indexOf(item) === i);
-//                         console.log(uniqe);
-//                         return uniqe;
-//                 }
-                    
-//                 }
-                
-//             });
-//         }
-//         )
-// }
-
-
-// console.log(searchGenresName(35,28));
+function clearGallery() {
+    galleryEl.innerHTML = '';
+}
